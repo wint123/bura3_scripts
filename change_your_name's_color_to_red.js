563807090f31936c51e88c6e5a72cd992b68a3f1
Wint.* <mrball-123.09128@auone.jp>
2014-02-03 14:17:54 +0900

    Update change_your_name's_color_to_red.js

--- change_your_name's_color_to_red.js
+++ change_your_name's_color_to_red.js
@@ -1,22 +1,20 @@
 // ==UserScript==
 // @name Bura3 how many Resources - Wint
 // @namespace http://wint.x0.com/
-// @description どれだけの資源を持っているのか表示します
-// @include http://s25.3gokushi.jp/*
+// @description あなたの名前を赤にします。
+// @include http://*.3gokushi.jp/*
+// @include https://*.3gokushi.jp/*
 // ==/UserScript==
 
 (function() {
+	var your_id = ""; //あなたのブラウザ三国志のユーザー番号を入力してください。
 	var name = document.getElementsByTagName("a");
 	console.log(name.length);
 
 	for(i=1;i<name.length;i++){
 		var name_href = name[i].getAttribute('href');
-		if(name_href == "/user/?user_id=1675"){
+		if(name_href == "/user/?user_id=" + your_id){
 			name[i].style.color = "#FF0000";
-			console.log("一致しました！");
-		}
-		if(name_href == "/user/?user_id=1775"){
-			name[i].style.color = "#00FF00";
 			console.log("一致しました！");
 		}
 		console.log(i + "回目の実行です！");
